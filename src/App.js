3b1e4fa3319311407390c1fa59569f25ece7365e
Nazmul Alom <nazmulshuvo03@gmail.com>
2019-04-13 02:11:25 +0600

    hashrouter converted to browserrouter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React, { Component } from "react";
-import { HashRouter, Switch, Route } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import Post from "./components/Post";
-import View from "./components/View";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import Post from './components/Post';
+import View from './components/View';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 class App extends Component {
-  render() {
-    return (
-      <HashRouter>
-        <div className="container-fluid">
-          <Navbar />
-          <Switch>
-            <Route exact path="/" component={View} />
-            <Route path="/post" component={Post} />
-          </Switch>
-          <Footer />
-        </div>
-      </HashRouter>
-    );
-  }
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="container-fluid">
+					<Navbar />
+					<Switch>
+						<Route exact path="/" component={View} />
+						<Route path="/post" component={Post} />
+					</Switch>
+					<Footer />
+				</div>
+			</BrowserRouter>
+		);
+	}
 }
 
 export default App;
