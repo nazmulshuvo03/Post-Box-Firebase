e3f8d252328a20308091920172e9b9e40c8f0884
Nazmul Alom <nazmulshuvo03@gmail.com>
2019-04-13 02:21:49 +0600

    design change

--- src/components/View.js
+++ src/components/View.js
@@ -1,43 +1,46 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { firestoreConnect } from 'react-redux-firebase';
-import { compose } from 'redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { firestoreConnect } from "react-redux-firebase";
+import { compose } from "redux";
 
 class View extends Component {
-	render() {
-		const { posts } = this.props;
-		const postList =
-			posts &&
-			posts.map((post) => {
-				//console.log(post);
-				return (
-					<div key={post.id} className="card z-depth-5">
-						<div className="card-content">
-							<div className="card-title">
-								<span>{post.title}</span>
-							</div>
-							<div className="card-body">
-								<p>{post.content}</p>
-							</div>
-						</div>
-					</div>
-				);
-			});
-		return (
-			<div className="post-list container z-depth-5 white">
-				<h3 className="grey-text center">Recent Posts</h3>
-				<div className="posts">{postList}</div>
-			</div>
-		);
-	}
+  render() {
+    const { posts } = this.props;
+    const postList =
+      posts &&
+      posts.map(post => {
+        //console.log(post.id);
+        return (
+          <div key={post.id} className="card z-depth-5">
+            <div className="card-content">
+              <div className="card-title">
+                <span>{post.title}</span>
+              </div>
+              <div className="card-body">
+                <p>{post.content}</p>
+              </div>
+            </div>
+          </div>
+        );
+      });
+    return (
+      <div className="post-list container z-depth-5 white">
+        <h3 className="grey-text center">Recent Posts</h3>
+        <div className="posts">{postList}</div>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps = (state) => {
-	//console.log(state.firestore.data);
-	return {
-		//posts: state.post.posts
-		posts: state.firestore.ordered.posts
-	};
+const mapStateToProps = state => {
+  //console.log(state.firestore.data);
+  return {
+    //posts: state.post.posts
+    posts: state.firestore.ordered.posts
+  };
 };
 
-export default compose(connect(mapStateToProps), firestoreConnect([ { collection: 'posts' } ]))(View);
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: "posts" }])
+)(View);
